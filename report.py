b2c23b1df16633c203294c1667bf7de45f196a2c
D3V1D <98307570+D3ViD@users.noreply.github.com>
2022-02-08 00:34:34 +0600

    Add files via upload

---
+++ report.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Name:D3V1D
+#This Coad Written By Sagor Vau 
+#D3V1D 
+
+import time
+import os
+
+os.system('clear')
+time.sleep(0.5)
+try:
+    import mechanize
+except ModuleNotFoundError:
+    print '[!] Module >Mechanize< Not Found!\n    This module is only available in python 2.x :/\n    Please install mechanize (pip install mechanize) and run the program with python2'
+    exit()
+os.system("clear")
+print """
+\033[1;32m######  ######
+\033[1;32m#     # #     #
+\033[1;32m#     # #     #
+\033[1;32m#     # ######
+\033[1;32m#     # #   #
+\033[1;32m#     # #    #
+\033[1;32m######  #     #
+\033[1;34m    ____                        __
+\033[1;34m   / __ \___  ____  ____  _____/ /____  _____
+\033[1;34m  / /_/ / _ \/ __ \/ __ \/ ___/ __/ _ \/ ___/
+\033[1;34m / _, _/  __/ /_/ / /_/ / /  / /_/  __/ /
+\033[1;34m/_/ |_|\___/ .___/\____/_/   \__/\___/_/
+\033[1;34m          /_/
+
+\033[1;91m           [☠️\033[1;91mAuthor Name: Nazmul Huda      ☠️\033[1;91m]
+\033[1;91m           [☠️\033[1;91mPage       : Md. Sumon   ☠️\033[1;91m]
+\033[1;91m           [☠️\033[1;91mYutube Chnl: Nazmul Huda  ☠️\033[1;91m]
+\033[1;91m           [☠️    \033[1;91mCountry: Bangladesh      ☠️\033[1;91m]
+
+\033[1;43m\033[1;37m         😈 PUT ACCOUNT USER ID FOR REPORT 😈           \033[1;0m
+"""
+time.sleep(0.5)
+user = raw_input('[+] ENTER USER ID FOR UNLIMITED REPORTS 😈💉 : ')
+time.sleep(1)
+print '\033[1;32m[+] 1 Report  Send Successfully '
+time.sleep(4)
+print '\033[1;32m[+] 2 Reports Send Successfully '
+time.sleep(3)
+print '\033[1;32m[+] 3 Reports Send Successfully '
+time.sleep(5)
+print '\033[1;32m[+] 4 Reports Send Successfully '
+time.sleep(1)
+print '\033[1;32m[+] 5 Reports Send Successfully '
+time.sleep(0.5)
+print '\033[1;32m[+] 6 Reports Send Successfully '
+time.sleep(1)
+print '\033[1;32m[+] 7 Reports Send Successfully '
+time.sleep(0.3)
+print '\033[1;32m[+] 8 Reports Send Successfully '
+time.sleep(1)
+print '\033[1;32m[+] 9 Reports Send Successfully '
+time.sleep(2)
+print '\033[1;32m[+]10 Reports Send Successfully '
+time.sleep(0.9)
+print '\033[1;32m[+]11 Reports Send Successfully '
+time.sleep(0.2)
+print '\033[1;32m[+]12 Reports Send Successfully '
+time.sleep(1)
+print '\033[1;32m[+]13 Reports Send Successfully '
+time.sleep(1)
+print '\033[1;32m[+]14 Reports Send Successfully '
+time.sleep(1.2)
+print '\033[1;32m[+]15 Reports Send Successfully '
+time.sleep(0.7)
+print '\033[1;32m[+]16 Reports Send Successfully '
+time.sleep(0.2)
+print '\033[1;32m[+]17 Reports Send Successfully '
+time.sleep(1)
+print '\033[1;32m[+]18 Reports Send Successfully '
+time.sleep(0.2)
+print '\033[1;32m[+]19 Reports Send Successfully '
+time.sleep(0.1)
+print '\033[1;32m[+]20 Reports Send Successfully                                        '
+time.sleep(0.1)
+print '\033[1;32m[+]\033[1;33mFew Reports Has Been Sended Successfully'  
+time.sleep(3)
+print '\033[1;32m[+]\033[1;31mMore Reports in Progress    '  
+time.sleep(1)
+print '\033[1;32m[+]Please Wait... '
+time.sleep(20)
+print '\033[1;32m[+]All Reports Has Been Sended Successfully '
+exit()          
